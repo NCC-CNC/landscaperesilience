69d85b5ebfdf215892bef0a7df7b450fd886683f
Dan Wismer <Daniel.Wismer@natureconservancy.ca>
2023-12-15 09:51:42 -0700

    landR histogram

---
+++ R/fct_landr_hist.R
@@ -0,0 +1,75 @@
+#' landr_hist_bits
+#'
+#' @description A fct function
+#'
+#' @return The return value, if any, from executing the function.
+#'
+#' @noRd
+#' 
+#' @export
+#' 
+
+landr_hist_bits <- function(landr_tbl=NULL, oid=NULL) {
+  
+  if(is.null(landr_tbl)) {
+    # get lanrR values, dummy data
+    landr_values <- runif(15, min = 0, max = 3) %>% round(2)
+  } else {
+    # get landR values
+    landr_values <- unname(unlist(landr_tbl[[oid]][1]))
+  }
+  
+  # get mean value
+  mean_value <- mean(landr_values) %>% round(2)
+  
+  # build histogram table info
+  bins <- seq(0, 4, by = 0.2) 
+  bin_counts <- cut(landr_values, bins, right = FALSE) 
+  df <- data.frame(table(bin_counts))
+  df$Bins <- as.character(counts_df$bin_counts)
+  df$Count <- counts_df$Freq
+  # Generate labels for the bins
+  df$Xlabel <- gsub(",", "-", substring(as.character(counts_df$Bins), 2, nchar(as.character(counts_df$Bins)) - 1))
+  
+  return(list(df, mean_value, bins))
+}
+
+
+#' empty_landr_hist
+#'
+#' @description A fct function
+#'
+#' @return The return value, if any, from executing the function.
+#'
+#' @noRd
+#' 
+#' @export
+#' 
+empty_landr_hist <- function(landr_hist_df) {
+
+  # build bar chart disguised as a histogram. Include chart elements for proxy. 
+  bar_chart <- highcharter::highchart() %>%
+    highcharter::hc_chart(type = "column") %>%
+    highcharter::hc_add_series(
+      data = NULL, # <- this makes the chart empty
+      pointPadding = 0, groupPadding = 0, borderWidth = 0, pointWidth = 10,
+      color = "#33862B") %>%
+    highcharter::hc_xAxis(
+      title = list(text = "Resilience Scores"),
+      categories = landr_hist_df$Xlabel,
+      tickWidth = 1,     
+      tickColor = "#000"
+      ) %>%
+    highcharter::hc_subtitle(text = "Score Distribution") %>%
+    highcharter::hc_yAxis(
+      title = list(text = "Area (km2)")) %>%
+    highcharter::hc_xAxis(
+      title = list(text = "Resilience Scores")) %>%
+    highcharter::hc_tooltip(
+      formatter = JS(
+        "function() { return 'Score Range: ' + this.x + '<br>Area (km2): ' + this.y; }"
+        )) %>%
+    highcharter::hc_legend(enabled = FALSE)
+  
+  return(bar_chart)
+}
---
+++ R/mod_histogram_popup.R
@@ -0,0 +1,70 @@
+#' histogram_popup UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd 
+#'
+#' @importFrom shiny NS tagList 
+mod_histogram_popup_ui <- function(id){
+  ns <- NS(id)
+  tagList(
+    highcharter::highchartOutput(outputId = ns("histpopup"), height = "400px")
+  )
+}
+    
+#' histogram_popup Server Functions
+#'
+#' @noRd 
+mod_histogram_popup_server <- function(id, landr_tbl, oid = NULL, n_click){
+  moduleServer(id, function(input, output, session){
+    ns <- session$ns
+
+    if (is.null(oid)) {
+      
+      # build empty histogram df
+      landr_hist_bits <- landr_hist_bits(landr_tbl=NULL, oid=NULL)
+      landr_df <- landr_hist_bits[[1]] 
+      
+      # render empty histogram
+      h <- empty_landr_hist(landr_df)
+      output$histpopup <- highcharter::renderHighchart({ h })
+      
+      } else {
+      
+      # build landR histogram df
+      landr_hist_bits <- landr_hist_bits(landr_tbl=landr_tbl, oid=oid)
+      landr_df <- landr_hist_bits[[1]]
+      landr_mean <- landr_hist_bits[[2]]
+      bins <- landr_hist_bits[[3]]
+      
+      # Update histogram  
+      highcharter::highchartProxy("histogram_popup_1-histpopup") %>%
+        highcharter::hcpxy_remove_series(all=TRUE) %>%
+        highcharter::hcpxy_add_series(
+          type= "column",
+          data = landr_df,
+          pointPadding = 0, groupPadding = 0, borderWidth = 0, pointWidth = 10,
+          color = "#33862B",
+          mapping = highcharter::hcaes(x = Xlabel, y = Count)
+          ) %>%
+        highcharter::hcpxy_update(
+          xAxis = list(plotLines = list(list(
+          value = findInterval(landr_mean, bins) - 1,
+          color = '#FF5B00',
+          width = 1,
+          zIndex = 4,
+          label = list(
+            text = paste0("mean: ", mean_value),
+            style = list( color = '#FF5B00', fontWeight = 'bold')))))
+        ) 
+      }
+  })
+}
+    
+## To be copied in the UI
+# mod_histogram_popup_ui("histogram_popup_1")
+    
+## To be copied in the server
+# mod_histogram_popup_server("histogram_popup_1")
