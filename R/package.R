879637990ab0075c71973ebd165a3e04e5a9077c
Dan Wismer <Daniel.Wismer@natureconservancy.ca>
2024-01-11 09:52:13 -0700

    import depends
    need to define imports in DESCRIPTION and the package.R file, then devtools::document() to update NAMESPACE file.

---
+++ R/package.R
@@ -0,0 +1,29 @@
+#' @import bsicons
+#' @import bslib
+#' @import dplyr
+#' @import exactextractr
+#' @import geojsonsf
+#' @import ggplot2
+#' @import htmltools
+#' @import openxlsx 
+#' @import sf
+#' @import shiny
+#' @import terra
+NULL
+
+#' landscaperesilience:
+#'
+#' TO DO
+#'
+#' @name landscaperesilience
+#'
+#' @docType package
+#'
+#' @examples
+#' \donttest{
+#  # launch application
+#' if (interactive()) {
+#' run_app()
+#' }
+#' }
+NULL
