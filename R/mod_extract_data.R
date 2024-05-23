e466d6c5dd51f6f0bc02cc80b9e7e6256c4b7b95
Dan Wismer <daniel.s.wismer@gmail.com>
2024-05-23 14:43:22 -0600

    build LandR on the fly
    - extract normalized variables
    - extract raw variables
    - build LandR
    - build % contribution to LandR

--- R/mod_extract_data.R
+++ R/mod_extract_data.R
@@ -54,8 +54,8 @@ mod_extract_data_server <- function(id, user_poly, wtw_path, shp_name_field, tif
            }
            
            # Project to Canada_Albers_WGS_1984
-           if (st_crs(user_poly) != st_crs(input_data$landr$layer)) {
-             user_poly_prj <- st_transform(user_poly(), crs= st_crs(input_data$landr$layer))
+           if (st_crs(user_poly) != st_crs(input_data$landr)) {
+             user_poly_prj <- st_transform(user_poly(), crs= st_crs(input_data$landr))
            }
            
            # Create ObjectId
@@ -68,41 +68,72 @@ mod_extract_data_server <- function(id, user_poly, wtw_path, shp_name_field, tif
            incProgress(2)
            id_ <- showNotification("Extracting impact metrics ...", duration = 0, closeButton=close)
            
-           ## Extract national data: MAX
-           max_vars <- rlist::list.filter(input_data,(fun == "max"))
-           max_layers <- rlist::list.mapv(max_vars, layer)
-           max_shp_name <- rlist::list.mapv(max_vars, shp_name)
-           extracted_max <- exact_extract(rast(max_layers), user_poly_prj, fun = "max", force_df = TRUE) %>% round(2)
-           names(extracted_max) <- gsub("max.", "", max_shp_name)
-           extracted_max[is.na(extracted_max)] <- 0 # replace NA with 0 
+           ## Extract normalized variables (lr_ready), sum
+           lr_ready_layers <- rlist::list.mapv(input_data$lr_ready, layer)
+           lr_ready_shp_name <- rlist::list.mapv(input_data$lr_ready, shp_name)
+           extracted_lr_ready <- exact_extract(rast(lr_ready_layers), user_poly_prj, fun = "sum", force_df = TRUE) %>% round(2)
+           names(extracted_lr_ready) <- gsub("sum.", "", lr_ready_shp_name)
+           extracted_lr_ready[is.na(extracted_lr_ready)] <- 0 # replace NA with 0 
            
-           ## Extract national data: SUM
-           sum_vars <- rlist::list.filter(input_data,(fun == "sum"))
+           ## Extract raw variables, sum
+           sum_vars <- rlist::list.filter(input_data$lr_raw,(fun == "sum"))
            sum_layers <- rlist::list.mapv(sum_vars, layer)
            sum_shp_name <- rlist::list.mapv(sum_vars, shp_name)
            extracted_sum <- exact_extract(rast(sum_layers), user_poly_prj, fun = "sum", force_df = TRUE) %>% round(2)
            names(extracted_sum) <- gsub("sum.", "", sum_shp_name)
-           extracted_sum[is.na(extracted_sum)] <- 0 # replace NA with 0   
+           extracted_sum[is.na(extracted_sum)] <- 0 # replace NA with 0 
+           
+           ## Extract raw variables, max
+           max_vars <- rlist::list.filter(input_data$lr_raw,(fun == "max"))
+           max_layers <- rlist::list.mapv(max_vars, layer)
+           max_shp_name <- rlist::list.mapv(max_vars, shp_name)
+           extracted_max <- exact_extract(rast(max_layers), user_poly_prj, fun = "max", force_df = TRUE) %>% round(2)
+           names(extracted_max) <- gsub("max.", "", max_shp_name)
+           extracted_max[is.na(extracted_max)] <- 0 # replace NA with 0             
            
            ## Extract Landscape Resilience distribution
-           # Message
            incProgress(3)
            removeNotification(id_)
            id_ <- showNotification("Extracting resilience ...", duration = 0, closeButton=close)
-           
-           user_poly_prj$LANDR_SUM <- exact_extract(input_data$landr$layer, user_poly_prj, 'sum') %>% round(2)
-           user_poly_prj$LANDR_MIN <- exact_extract(input_data$landr$layer, user_poly_prj, 'min') %>% round(2)
-           user_poly_prj$LANDR_MU <- exact_extract(input_data$landr$layer, user_poly_prj, 'mean') %>% round(2)
-           user_poly_prj$LANDR_MED <- exact_extract(input_data$landr$layer, user_poly_prj, 'median') %>% round(2)
-           user_poly_prj$LANDR_MAX <- exact_extract(input_data$landr$layer, user_poly_prj, 'max') %>% round(2)
-           user_poly_prj$LANDR_SD <- exact_extract(input_data$landr$layer, user_poly_prj, 'stdev') %>% round(2)
-           landr_tbl <- exact_extract(input_data$landr$layer, user_poly_prj) 
-           
-           # Round all values
+           landr_tbl <- exact_extract(input_data$landr, user_poly_prj) 
            landr_tbl <-round_nested_list(landr_tbl) # fct_round_nested_list.R
+           
+           ## Calculate Landscape Resilience on the fly
+           extracted_lr_ready <- extracted_lr_ready  %>%
+             mutate(
+               LANDR_POS =
+                 nCH +
+                 nBIOD_GOAL + nEND_GOAL + nSAR_GOAL + 
+                 nBIOD_RICH + nEND_RICH + nSAR_RICH +
+                 nCLIMATE_C + nCLIMATE_R +
+                 nCONNECT +
+                 nFOREST + nGRASS + nWET + nSHORE
+             ) %>%
+             mutate(LANDR_NEG = nCLIMATE_E + nHFI) %>%
+             mutate(LANDR_ABS = LANDR_POS + LANDR_NEG) %>%
+             mutate(LANDR = LANDR_POS - LANDR_NEG, .before=1)
+           
+           ## Calculate contribution %
+           extracted_lr_ready <- extracted_lr_ready %>%
+             mutate(pCH = (nCH / LANDR_ABS) * 100) %>%
+             mutate(pBIOD_GOAL = (nBIOD_GOAL / LANDR_ABS) * 100) %>%
+             mutate(pEND_GOAL = (nEND_GOAL / LANDR_ABS) * 100) %>%
+             mutate(pSAR_GOAL = (nSAR_GOAL / LANDR_ABS) * 100) %>%
+             mutate(pBIOD_RICH = (nBIOD_RICH / LANDR_ABS) * 100) %>%
+             mutate(pEND_RICH = (nEND_RICH / LANDR_ABS) * 100) %>%
+             mutate(pSAR_RICH = (nSAR_RICH / LANDR_ABS) * 100) %>%
+             mutate(pCLIMATE_C = (nCLIMATE_C / LANDR_ABS) * 100) %>%
+             mutate(pCLIMATE_R = (nCLIMATE_R / LANDR_ABS) * 100) %>%
+             mutate(pCONNECT = (nCONNECT / LANDR_ABS) * 100) %>%
+             mutate(pFOREST = (nFOREST / LANDR_ABS) * 100) %>%
+             mutate(pGRASS = (nGRASS / LANDR_ABS) * 100) %>%
+             mutate(pWET = (nWET / LANDR_ABS) * 100) %>%
+             mutate(pSHORE = (nSHORE / LANDR_ABS) * 100) %>%
+             mutate(pCLIMATE_E = (nCLIMATE_E / LANDR_ABS) * 100) %>%
+             mutate(pHFI = (nHFI / LANDR_ABS) * 100) %>%
+             mutate(across(where(is.numeric), round, digits = 2))
           
            # Extract user .tif(s)
-           # Message
            incProgress(4)
            removeNotification(id_)
            id_ <- showNotification("Extracting .tifs ...", duration = 0, closeButton=close)
@@ -122,8 +153,10 @@ mod_extract_data_server <- function(id, user_poly, wtw_path, shp_name_field, tif
              }
            }
            
-           # Combine extractions into on sf object
-           user_poly_prj <- cbind(user_poly_prj, extracted_max, extracted_sum)
+           # Combine extractions into one sf object
+           user_poly_prj <- cbind(user_poly_prj, extracted_max, extracted_sum, extracted_lr_ready) %>%
+             relocate(LANDR, .after = AREA_HA) %>%
+             mutate(rLANDR = round(LANDR / AREA_HA, 4), .after = LANDR)
            
            # Project to WGS
            user_poly_wgs <- st_transform(user_poly_prj, crs=4326)
@@ -134,7 +167,6 @@ mod_extract_data_server <- function(id, user_poly, wtw_path, shp_name_field, tif
            to_return$user_poly_display <- user_poly_wgs
            to_return$landr_tbl <- landr_tbl
            
-           
            # Send to client
            send_geojson(
              session, 
@@ -144,7 +176,7 @@ mod_extract_data_server <- function(id, user_poly, wtw_path, shp_name_field, tif
              shp_name_field = shp_name_field()
             )
            
-           # enable metrics picker in card
+           # Enable metrics picker in card
            shinyjs::enable("metrics_bar_1-impact", asis = TRUE)
 
            # Finish progress bar
